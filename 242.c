eba0240266dae03d816ab56f2fe13e1f03031ba1
longing <du_303412@163.com>
2016-03-04 11:08:51 +0800

    Valid Anagram --my bad code, its not clean

---
+++ 242.c
@@ -0,0 +1,44 @@
+/*************************************************************************
+    > File Name: 242.c
+    > Author: duqinglong
+    > Mail: du_303412@163.com 
+    > Created Time: Fri 04 Mar 2016 11:06:51 AM CST
+ ************************************************************************/
+bool isAnagram(char* s, char* t) {
+	int lengthS, lengthT;
+	if (s == NULL && t == NULL)
+		return true;
+	else if ((s && !t) || (!s && t))
+		return false;
+	lengthS = strlen(s), lengthT = strlen(t);
+	if (lengthS != lengthT)
+		return false;
+	sortString(s);
+	sortString(t);
+	while (*s && *t)
+	{
+		if (*s != *t)
+			return false;
+		*s++, *t++;
+	}
+	return true;
+}
+
+void sortString(char* s)
+{
+	int lengthS, i, j;
+	char temp;
+	lengthS = getLength(s);
+	for (i = 0; i < lengthS; i++)
+	{
+		for (j = 0; j < lengthS - i - 1; j++)
+		{
+			if (s[j] > s[j+1])
+			{
+				temp = s[j];
+				s[j] = s[j+1];
+				s[j+1] = temp;
+			}
+		}
+	}
+}
